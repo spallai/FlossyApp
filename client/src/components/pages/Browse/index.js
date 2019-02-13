80724abf2ef0a80fcc5a65c9e2cb73303797c5bc
Devon Burdick <Devonmburdick@gmail.com>
2019-02-12 17:53:50 -0800

    added some opacity

---
+++ client/src/components/pages/Browse/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Browse extends React.Component {
+  state = {
+  };
+
+  render() {
+    return (
+    <h2>Hello I am the Browse page!!!!</h2>
+    );
+  }
+}
+
+export default Browse;
