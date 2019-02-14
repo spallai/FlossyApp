612320709dca5fc389e2948d143b5b0d87caf07b
Devon Burdick <Devonmburdick@gmail.com>
2019-02-14 11:31:21 -0800

    Heroku deploy

---
+++ client/build/precache-manifest.4f5b8d2d23a6a9c028784bbd8997bc2d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d005484c601edc57e961ae311b5473af",
+    "url": "/DevonBurdick/FlossyApp/static/media/followingpic4.d005484c.jpg"
+  },
+  {
+    "revision": "c08c8978349ded27c53c",
+    "url": "/DevonBurdick/FlossyApp/static/css/main.10029c16.chunk.css"
+  },
+  {
+    "revision": "625344c726863d563e29",
+    "url": "/DevonBurdick/FlossyApp/static/js/1.625344c7.chunk.js"
+  },
+  {
+    "revision": "8cfb20a0e389bb2ff9bc",
+    "url": "/DevonBurdick/FlossyApp/static/js/runtime~main.8cfb20a0.js"
+  },
+  {
+    "revision": "2d89e56ca197d6c849c8a8462254c160",
+    "url": "/DevonBurdick/FlossyApp/static/media/userpic.2d89e56c.jpeg"
+  },
+  {
+    "revision": "2f29380656b29b3998382f988f032c81",
+    "url": "/DevonBurdick/FlossyApp/static/media/iconset.2f293806.jpeg"
+  },
+  {
+    "revision": "fb5a22e7b8e55829571ad890a6b6438a",
+    "url": "/DevonBurdick/FlossyApp/static/media/followingpic1.fb5a22e7.jpg"
+  },
+  {
+    "revision": "ac1d2aa75335c3c431d81fdcb0617435",
+    "url": "/DevonBurdick/FlossyApp/static/media/followingpic2.ac1d2aa7.jpg"
+  },
+  {
+    "revision": "301cb7da14e681ba03f22cce998450a7",
+    "url": "/DevonBurdick/FlossyApp/static/media/followingpic3.301cb7da.png"
+  },
+  {
+    "revision": "c08c8978349ded27c53c",
+    "url": "/DevonBurdick/FlossyApp/static/js/main.c08c8978.chunk.js"
+  },
+  {
+    "revision": "d319a7754da9a24ccbd79eed6bf18f29",
+    "url": "/DevonBurdick/FlossyApp/static/media/followpic1.d319a775.jpg"
+  },
+  {
+    "revision": "b91715db567c17ce8fffd8f37d459ef8",
+    "url": "/DevonBurdick/FlossyApp/static/media/followpic3.b91715db.jpg"
+  },
+  {
+    "revision": "af31028a1334a11bbe7f3fd65e09cb39",
+    "url": "/DevonBurdick/FlossyApp/static/media/followpic4.af31028a.jpg"
+  },
+  {
+    "revision": "fa13005154d41d15faa3f0424a06cd31",
+    "url": "/DevonBurdick/FlossyApp/static/media/feed1.fa130051.jpeg"
+  },
+  {
+    "revision": "fe92b5b603a840c3df7c39bb9881466e",
+    "url": "/DevonBurdick/FlossyApp/static/media/feed2.fe92b5b6.jpeg"
+  },
+  {
+    "revision": "697563e5633c7c2a3ede2206d491e673",
+    "url": "/DevonBurdick/FlossyApp/static/media/feed3.697563e5.jpeg"
+  },
+  {
+    "revision": "a6ca38c657f882b2cc75a64ee496e077",
+    "url": "/DevonBurdick/FlossyApp/static/media/background.a6ca38c6.jpeg"
+  },
+  {
+    "revision": "0effa6655b9b4d8a22cc614dff4806a0",
+    "url": "/DevonBurdick/FlossyApp/index.html"
+  }
+];
