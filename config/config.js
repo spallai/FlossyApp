26c927435af0c19302692446656468bb1681ac13
SPallai <sangeeta.sumair@gmail.com>
2019-02-13 21:03:31 -0800

    testing deploy

--- config/config.js
+++ config/config.js
@@ -1,6 +1,13 @@
 
 
 module.exports = {
-    db: 'mongodb://username:password@url:port/db',
-    db_dev: 'mongodb://localhost:3000/flossydb',
+    db: process.env.MONGODB_URI,
+    db_dev: process.env.MONGODB_URI
 };
+
+
+// module.exports = {
+//     db: 'mongodb://username:password@url:port/db',
+//     db_dev: 'mongodb://localhost:3000/flossydb',
+// };
+
