cabe356535227833a3d2fb4514d8abae19eca220
Devon Burdick <Devonmburdick@gmail.com>
2019-02-14 10:20:53 -0800

    Changed config and shanged to server.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,11 @@
 
-
 module.exports = {
-    db: 'mongodb://username:password@url:port/db',
-    db_dev: 'mongodb://localhost:3000/flossydb',
+    db: process.env.MONGODB_URI,
+    db_dev: 'mongodb://localhost:3000/flossydb'
 };
+
+
+// module.exports = {
+//     db: 'mongodb://username:password@url:port/db',
+//     db_dev: 'mongodb://localhost:3000/flossydb',
+// };
